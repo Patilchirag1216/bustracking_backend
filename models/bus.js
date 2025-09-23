705cce365d5407b40750d9f8d01d1cfd68cb7735
patilchirag1216 <patilchirag4220@gmail.com>
2025-09-23 11:22:27 +0530

    Modified user and bus schemas: added busId for conductor, updated bus schema with routename, busno, timing, isOccupied, location, conductorId

--- models/bus.js
+++ models/bus.js
@@ -1,3 +1,32 @@
 import mongoose from "mongoose";
 
- 
+const busSchema = new mongoose.Schema({
+  routeName: {
+    type: String,
+    required: true, // e.g. "City Center to Railway Station"
+  },
+  busNo: {
+    type: String,
+    required: true, // e.g. "MH54 B 2023"
+    unique: true,
+  },
+  timing: {
+    type: String,
+    required: true, // e.g. "9:00 AM - 11:00 AM"
+  },
+  conductorId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // links to conductor from user collection
+    default: null,
+  },
+  isOccupied: {
+    type: Boolean,
+    default: false,
+  },
+  location: {
+    lat: { type: Number, default: null },
+    lng: { type: Number, default: null },
+  },
+});
+
+export default mongoose.model("Bus",busSchema);
--- models/user.js
+++ models/user.js
@@ -15,7 +15,12 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  access: { type: String, enum: ["user", "conductor"], default: "user" }  
+  access: { type: String, enum: ["user", "conductor","admin"], default: "user" },
+   busId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Bus",
+    default: null,
+  },
 });
 
 export default mongoose.model("User", userSchema);
